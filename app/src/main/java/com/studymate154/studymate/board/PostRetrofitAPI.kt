4430085ac6a83ef14ee315b106e95bfb84730696
park-hwaan <parkhwan1129@gmail.com>
2024-07-26 23:20:47 +0900

    design/#71 SearchFragment UI 수정

--- app/src/main/java/com/studymate154/studymate/board/PostRetrofitAPI.kt
+++ app/src/main/java/com/studymate154/studymate/board/PostRetrofitAPI.kt
@@ -7,8 +7,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object PostRetrofitAPI {
-    private const val BASE_URL = "http://10.0.2.2:8080"
-
+    // 로컬 : http://10.0.2.2:8080
+    private const val BASE_URL = "https://studymate154.kro.kr"
 
 
     private val okHttpClient: OkHttpClient by lazy {
